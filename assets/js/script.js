c723d24b7947cfbac4bf07ee6a3a373ddc606b60
insomniKat <ru3ykat@gmail.com>
2021-09-26 09:47:59 -0400

    code review

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 var actHour="";
 var assignHour="";
+let currentHourIndex= 0;
 
 // get current date in day, month date
 var today = new Date();
@@ -7,13 +8,13 @@ var date =  today.getDate();
 
 // get current day by name
 var dayName = today.getDay();
-var dayList = ["Sunday","Monday","Tuesday","Wednesday","Thursday",
-"Friday","Saturday"];
+var dayList = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", 
+"Friday", "Saturday"];
 
 // get current month by name
 var monthName = today.getMonth();    
-var monthList = ["January","February","March","April","May","June","July","August",
-"September","October","November","December"];
+var monthList = ["January", "February", "March", "April", "May", "June", "July", "August", 
+"September", "October", "November", "December"];
 
 // define current date
 var todaysDate = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
@@ -25,30 +26,58 @@ var dateTime = todaysDate + ' ' + time;
 console.dir(actHour);
 console.dir(dateTime);
 
+// apply current date in "day, month date" format 
 document.getElementById("currentDay").textContent = dayList[dayName] + ", " + monthList[monthName] + " " + date;
 
-var silly = "9";
-var sillyTry = parseInt(silly);
+// set up array of static times for each hour in scheduler
+var timeBlock = [
+    { hour: 9, time: '09:00 AM', assign: '9'},
+    { hour: 10, time: '10:00 AM', assign: '10'},
+    { hour: 11, time: '11:00 AM', assign: '11'},
+    { hour: 12, time: '12:00 PM', assign: '12'},
+    { hour: 13, time: '01:00 PM', assign: '13'},
+    { hour: 14, time: '02:00 PM', assign: '14'},
+    { hour: 15, time: '03:00 PM', assign: '15'},
+    { hour: 16, time: '04:00 PM', assign: '16'}
+];
 
-console.dir(sillyTry);
-console.log(typeof(sillyTry));
+// dynamically create hour elements for each time-block
+var assignHour = $('#hour[i]');
+
+
+// need HELP here understanding syntax to associate timeBlock.assign value to #hour[i]
+// and then to use comparisons to addClass(), past/present/future to each hour
+$.each(timeBlock, function (i, value) {
+    assignHour[value.id] = value.assign;
+    if (assignHour > actHour) {
+        $()
+    }
+});
+
+
+// function setHour() {
+//     document.getElementById("hour")
+//     for()
+// }
+
+
+// function compareTime() {
+//     let actHour = Math.floor(today.getHours());
+
+//     for(i = 9; i <= 16; i++) {
+//         let timeBlock = $("#hour{i}");
+//     } if {
+//         actHour
+//     }
+// }
 
 // set scheduler ref-time for time comparison
-$('div#id')
 
-// USE JQUERY EACH() INSTEAD!  function compareTime() {
-//     var actHour = Math.floor(today.getHours());
-//     for (i = 8; i <= 16; i++) {
 
-//     }
-    
-//     var refTime = document.getElementsByName("ref-time");
-//     console.dir(refTime);
-// };
 // compareTime();
     // cycling through ref-time to compare and assign past/present/future classes
 
-
+// need HELP saving to localStorage and fetching saved array
 // save event text with save button (set each hour button to have unique id to store and retrieve)
     // save to local storage
 
