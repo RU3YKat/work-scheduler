c161d6003532a73c94909b73036a898ac3de77c5
insomniKat <ru3ykat@gmail.com>
2021-09-26 19:20:30 -0400

    add task persistence via localStorage on btn click

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,5 @@
 var localStorageId = "scheduleData";
-
-var actHour="";
 var assignHour="";
-let currentHourIndex= 0;
 
 // get current date in day, month date
 var today = new Date();
@@ -26,11 +23,10 @@ var hours = today.getHours();
 var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
 var dateTime = todaysDate + ' ' + time;
 
-console.dir(actHour);
-console.dir(dateTime);
-
 // apply current date in "day, month date" format 
 document.getElementById("currentDay").textContent = dayList[dayName] + ", " + monthList[monthName] + " " + date;
+
+
 
 // set up array of static times for each hour in scheduler
 var timeBlock = [
@@ -51,83 +47,50 @@ var foundPresent = false;
 
 var data = getStorage(localStorageId);
 
-
-// need HELP here understanding syntax to associate timeBlock.assign value to #hour[i]
+// dynamically assignHour to each hour.id
 // and then to use comparisons to addClass(), past/present/future to each hour
+
 $.each(assignHour, function (i, hour) {
-    //iterate through each hour
-  	//reminder: hours is already defined as today.getHours()
-  	//if the hour id contains the current hour, then set its class to "present"
-  	if (hour.id === "hour" + hours){ //if hours is 13, then "hour" + hours becomes "hour13"
-     	 $(hour).addClass("present");
-      	foundPresent = true;
-    }
-  	//if it's not...
-  	else {
-  		//if we haven't yet gotten to current hour, then set its class to "past"
-  		if (!foundPresent){
-        	$(hour).addClass("past");
+    // iterate through each hour
+    // if the hour id contains the current hour, then set its class to "present"
+    if(hour.id === "hour" + hours) {
+        // if hours is 13, then "hour" + hours becomes "hour13"
+        $(hour).addClass("present");
+        foundPresent = true;
+        // if it's not
+    } else { 
+        // if we haven't yet gotten to current hour, then set its class to "past"
+        if (!foundPresent) {
+            $(hour).addClass("past");
+        } else {
+        // else set its class to "future"
+        $(hour).addClass("future");
         }
-  		//else set its class to "future"
-      	else {
-        	$(hour).addClass("future");
-        }
-    }
-    // console.log(hour);
+    };
 
-    // deal with localStorage data
-  	if (data[hour.id]) {
-        console.log(data[hour.id]);
-
-    	$(hour).find("textarea").val (data[hour.id]);
-    }
-
-    $(hour).find("button").on("click", function(){
+    // add data persistence via localStorage for each hour block
+    if (data[hour.id]) {
+        $(hour).find("textarea").val(data[hour.id]);
+    };
+    // store appt data from textarea on button click
+    $(hour).find("button").on("click", function() {
         var appt = $(hour).find("textarea").val();
         data[hour.id] = appt;
         setStorage(localStorageId, data);
     });
 });
 
-
+// how to store data in localStorage
 function setStorage(id, data){
 	localStorage.setItem(id, JSON.stringify(data));
-}
+};
 
-function getStorage(id){
-	let data = localStorage.getItem(id);
-  	//if no data have been stored yet, then data will be undefined
-  	//change undefined to an empty object instead
-  	if (!data) return {};
-  	//localStorage can only store STRINGS, not objects
-  	return JSON.parse(data);
-}
-
-
-// function setHour() {
-//     document.getElementById("hour")
-//     for()
-// }
-
-
-// function compareTime() {
-//     let actHour = Math.floor(today.getHours());
-
-//     for(i = 9; i <= 16; i++) {
-//         let timeBlock = $("#hour{i}");
-//     } if {
-//         actHour
-//     }
-// }
-
-// set scheduler ref-time for time comparison
-
-
-// compareTime();
-    // cycling through ref-time to compare and assign past/present/future classes
-
-// need HELP saving to localStorage and reloading saved array
-// save event text with save button (set each hour button to have unique id to store and retrieve)
-    // save to local storage
-
-    // fetch from local storage
+// how to retrieve stored data from localStorage
+function getStorage(id) {
+    let data = localStorage.getItem(id);
+    // if no data have been stored yet, then data will be undefined
+    // change undefined to an empty object instead
+    if (!data) return {};
+    // localStorage stores strings, not objects
+    return JSON.parse(data);
+};
